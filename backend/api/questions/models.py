491ef91ed6582258df73403da78b15f65224cfa7
cyberseckenny <kennycs@protonmail.com>
2022-10-22 19:43:38 -0400

    Added secrets file to settings

--- backend/api/questions/models.py
+++ backend/api/questions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class QuestionModel(models.Model):
+    date = models.DateField(blank = False)
+    question = models.CharField(max_length = 300, blank = False)
+
+class AnswerModel(models.Model):
+    content = models.CharField(max_length = 500, blank=False)
+
+class UserModel(models.Model):
+    lastAnswer = models.ForeignKey(AnswerModel)
+    answers = models.ManyToManyField(AnswerModel)
+    macAddress = models.CharField(max_length = 12, blank = False)
