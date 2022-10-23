1d481fcb089b7846a8c3494bdbb758c140e3ada6
IsaacTrost <77888524+IsaacTrost@users.noreply.github.com>
2022-10-23 01:53:17 -0400

    CHARTS MFR

--- deepQuestion/www/js/graph.js
+++ deepQuestion/www/js/graph.js
@@ -19,32 +19,16 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-import "js/chart.js";
+import "./chart.js";
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['1','2','3','4','5','6','7','8','9','10','11','12', '13'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'contentness expressed',
+            data: [99,98, 45, 87, 67, 41, 34, 56, 99, 78, 3, 75, 24],
+            borderRadius: 7
         }]
     },
     options: {
