9f9ba2d932b99daf92adf25b4c7dba15cb3b1297
IsaacTrost <77888524+IsaacTrost@users.noreply.github.com>
2022-10-23 06:35:26 -0400

    did work with graph buttons

--- deepQuestion/www/js/graph.js
+++ deepQuestion/www/js/graph.js
@@ -20,17 +20,17 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 import "./chart.js";
-const ctx = document.getElementById('myChart').getContext('2d');
-var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+const ct1 = document.getElementById('graph').getContext('2d');
+var gradient = ct1.createLinearGradient(0, 0, 0, 400);
 gradient.addColorStop(0, 'rgba(59,160,249, 1)');   
 gradient.addColorStop(1, 'rgba(20,100,256,1)');
-const myChart = new Chart(ctx, {
+const myChart = new Chart(ct1, {
     type: 'bar',
     data: {
         labels: ['1','2','3','4','5','6','7','8','9','10','11','12', '13'],
         datasets: [{
             label: 'contentness expressed',
-            data: [99,98, 45, 87, 67, 41, 34, 56, 99, 78, 3, 75, 24],
+            data: [99,98, 45, 87, 67, 41, 34, 56, 99, 78, 3, 75, 24, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,],
             borderRadius: 6,
             backgroundColor: gradient,
             barPercentage: .95
@@ -61,4 +61,28 @@ const myChart = new Chart(ctx, {
     },
 
 });
+const wrapper = document.getElementsByName("graph-switch")
+const montheo = document.getElementById("monthbtn")
+const weekeo = document.getElementById("weekbtn")
+const yeareo = document.getElementById("yearbtn")
+
+function monthclick(){
+    wrapper.style.backgroundImage="radial-gradient(circle at 50% 50%, #3BA0F9, #F0F8FF);"
+    myChart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    myChart.update()
+}
+function weekclick(){
+    wrapper.style.backgroundImage="radial-gradient(circle at 16.666% 50%, #3BA0F9, #F0F8FF);"
+    myChart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    myChart.update()
+}
+function yearclick(){
+    wrapper.style.backgroundImage="radial-gradient(circle at 83.333% 50%, #3BA0F9, #F0F8FF);"
+    myChart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    myChart.update()
+}
+montheo.addEventListener("click", monthclick)
+montheo.addEventListener("click", weekclick)
+montheo.addEventListener("click", yearclick)
+
 
