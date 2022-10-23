11fae396770fff9468fe03582aa9ea68c02055c5
cyberseckenny <kennycs@protonmail.com>
2022-10-22 22:04:00 -0400

    Added api url

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -6,7 +6,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+Class-based viewpps
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('rest_framework.urls'))
 ]
