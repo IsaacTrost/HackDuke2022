37b9039063894742ffcd4766454470a417db3e24
IsaacTrost <77888524+IsaacTrost@users.noreply.github.com>
2022-10-23 07:58:08 -0400

    graphs changed

--- deepQuestion/www/js/graph.js
+++ deepQuestion/www/js/graph.js
@@ -67,17 +67,17 @@ const weekeo = document.getElementById("weekbtn")
 const yeareo = document.getElementById("yearbtn")
 
 function monthclick(){
-    wrapper.className = "graph-switch2"
+    wrapper.className = "graph-switch centery"
     myChart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
     myChart.update()
 }
 function weekclick(){
-    wrapper.className = "graph-switch1"
+    wrapper.className = "graph-switch lefty"
     myChart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
     myChart.update()
 }
 function yearclick(){
-    wrapper.className = "graph-switch3"
+    wrapper.className = "graph-switch righty"
     myChart.data.labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
     myChart.update()
 }
