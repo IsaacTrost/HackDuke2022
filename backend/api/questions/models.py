408f723ededb7bfd6c01d656757637e5489112c4
cyberseckenny <kennycs@protonmail.com>
2022-10-23 02:08:07 -0400

    Added tone to models

--- backend/api/questions/models.py
+++ backend/api/questions/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 
 class QuestionModel(models.Model):
-    date = models.DateField(blank = False)
+    date = models.DateTimeField(blank = False)
     question = models.CharField(max_length = 300, blank = False)
 
 class AnswerModel(models.Model):
     content = models.CharField(max_length = 500, blank=False)
+    tone = models.IntegerField(blank=False)
 
 class UserModel(models.Model):
     lastAnswer = models.ForeignKey(AnswerModel, on_delete=models.CASCADE)
